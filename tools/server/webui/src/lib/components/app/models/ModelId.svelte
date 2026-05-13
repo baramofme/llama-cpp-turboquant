9dfa7fa7d6e93438b9afd5cfe491e1802ac259ea
Aleksander Grygier <aleksander.grygier@gmail.com>
2026-05-13 16:39:36 +0200

    webui: Deduplicate model aliases in data + handle single/multiple aliases in UI (#22979)
    * fix: Deduplicate aliases + display single alias instead of default name or 2+ aliases as tags
    
    * refactor: Address review comments

--- tools/server/webui/src/lib/components/app/models/ModelId.svelte
+++ tools/server/webui/src/lib/components/app/models/ModelId.svelte
@@ -29,11 +29,12 @@
 
 	let parsed = $derived(ModelsService.parseModelId(modelId));
 	let resolvedShowRaw = $derived(showRaw ?? (config().showRawModelNames as boolean) ?? false);
-	let displayName = $derived(
-		aliases && aliases.length > 0 ? aliases[0] : (parsed.modelName ?? modelId)
-	);
-	let remainingAliases = $derived(aliases && aliases.length > 1 ? aliases.slice(1) : []);
-	let allTags = $derived([...(parsed.tags ?? []), ...(tags ?? [])]);
+
+	let uniqueAliases = $derived([...new Set(aliases ?? [])]);
+	let uniqueTags = $derived([...new Set([...(parsed.tags ?? []), ...(tags ?? [])])]);
+
+	let primaryAlias = $derived(uniqueAliases.length === 1 ? uniqueAliases[0] : null);
+	let displayName = $derived(primaryAlias ?? parsed.modelName ?? modelId);
 </script>
 
 {#if resolvedShowRaw}
@@ -56,14 +57,18 @@
 			</span>
 		{/if}
 
-		{#if remainingAliases.length > 0}
-			{#each remainingAliases as alias (alias)}
+		{#if primaryAlias}
+			{#if primaryAlias !== parsed.modelName}
+				<span class={badgeClass}>{parsed.modelName ?? modelId}</span>
+			{/if}
+		{:else if uniqueAliases.length > 1}
+			{#each uniqueAliases as alias (alias)}
 				<span class={badgeClass}>{alias}</span>
 			{/each}
 		{/if}
 
-		{#if allTags.length > 0}
-			{#each allTags as tag (tag)}
+		{#if uniqueTags.length > 0}
+			{#each uniqueTags as tag (tag)}
 				<span class={tagBadgeClass}>{tag}</span>
 			{/each}
 		{/if}
