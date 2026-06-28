8c9307e2cd84c58624fd92f4f31c7d54f3f992f1
baramofme <baramofme@gmail.com>
2026-06-28 15:10:00 +0900

    server: fix update_status call after struct refactor
    Assisted-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- tools/server/server-models.cpp
+++ tools/server/server-models.cpp
@@ -940,7 +940,7 @@ void server_models::load(const std::string & name, const load_options & opts) {
                 // Immediately mark UNLOADED so /v1/models stops advertising
                 // this model as loaded.
                 if (feof(stdout_file)) {
-                    this->update_status(name, SERVER_MODEL_STATUS_UNLOADED, 1);
+                    this->update_status(name, {SERVER_MODEL_STATUS_UNLOADED, 1});
                 }
             } else {
                 SRV_ERR("failed to get stdout/stderr of child process for name=%s\n", name.c_str());
